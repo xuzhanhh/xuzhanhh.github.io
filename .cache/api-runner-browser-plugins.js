9c1d4243680b7c96bc7927e9dacba024744c5764
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-19 16:42:32 +0000

    Deploy xuzhanhh/xuzhanhh.github.io to github.com/xuzhanhh/xuzhanhh.github.io.git:master

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/xuzhanhong1/study/overreacted.io/node_modules/gatsby-plugin-google-analytics/gatsby-browser'),
-      options: {"plugins":[],"trackingId":"UA-130227707-1"},
-    },{
-      plugin: require('/Users/xuzhanhong1/study/overreacted.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
